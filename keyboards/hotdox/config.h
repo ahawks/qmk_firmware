516f27f0e6cb8d6da09f81b6b1bd32a2f220ecc3
AJ Hawks <aj.hawks@egov.com>
2019-07-25 10:54:20 -0600

    Changing mouse key interval and keypress/hold limit

--- keyboards/hotdox/config.h
+++ keyboards/hotdox/config.h
@@ -15,7 +15,7 @@
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 14
 
-#define MOUSEKEY_INTERVAL       20
+#define MOUSEKEY_INTERVAL       16
 #define MOUSEKEY_DELAY          0
 #define MOUSEKEY_TIME_TO_MAX    60
 #define MOUSEKEY_MAX_SPEED      7
@@ -26,7 +26,7 @@
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
 
-#define TAPPING_TERM    140
+#define TAPPING_TERM    150
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
